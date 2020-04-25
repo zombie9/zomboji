24b5fbcb614325e16a64daf32038419e8e2be13e
zombie9 <darryl.banks@zombie9.com>
2020-04-25 11:57:12 +0100

    add Drawvetica font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './custom.scss';
+import './index.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
