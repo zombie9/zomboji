2c4dfeacae528b8ebb779f9b87747b522e8093ce
zombie9 <darryl.banks@zombie9.com>
2020-04-28 16:53:29 +0100

    add material ui sliders for customiser

---
+++ src/components/customiser/customiser-item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Slider } from '@material-ui/core'
+
+function Customiser() {
+  return (
+      <div>
+        <h5>{{}}</h5>
+        <Slider />
+      </div>
+  );
+}
+
+export default Customiser;
--- src/components/customiser/customiser.js
+++ src/components/customiser/customiser.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Slider } from '@material-ui/core'
 
 function Customiser() {
   return (
     <div className="d-flex align-items-center">
       <div>
         <h5>head</h5>
+        <Slider />
         <h5>eyes</h5>
+        <Slider />
         <h5>nose</h5>
+        <Slider />
         <h5>beard</h5>
+        <Slider />
       </div>
     </div>
   );
