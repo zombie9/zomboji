a5dba98c43b97ba661fbaf7435e3c5580c13e782
zombie9 <darryl.banks@zombie9.com>
2021-06-02 22:46:42 +0100

    Updates

---
+++ precache-manifest.8bba2386b6e608aeb3ae22d0946c13e9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4e3cd78c24d73cd0e718a995a81633",
+    "url": "/zomboji/index.html"
+  },
+  {
+    "revision": "4bc9be5d213cd67f5274",
+    "url": "/zomboji/static/css/main.8e363710.chunk.css"
+  },
+  {
+    "revision": "f3d66d1a449d6dd12c9d",
+    "url": "/zomboji/static/js/2.b4ab22f7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/zomboji/static/js/2.b4ab22f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc9be5d213cd67f5274",
+    "url": "/zomboji/static/js/main.cc553219.chunk.js"
+  },
+  {
+    "revision": "48f51143c1023ac8799b",
+    "url": "/zomboji/static/js/runtime-main.ed67281d.js"
+  },
+  {
+    "revision": "3b721e3bb01b565b1571ffb057803cca",
+    "url": "/zomboji/static/media/DrawveticaMini-webfont.3b721e3b.woff"
+  },
+  {
+    "revision": "2f0b7aef6f797e8d37d8e5b3e16714f7",
+    "url": "/zomboji/static/media/DrawveticaMini.2f0b7aef.ttf"
+  },
+  {
+    "revision": "45ea41eee2f2b6f9b7dffdcdac1748ff",
+    "url": "/zomboji/static/media/zomboji-1.45ea41ee.png"
+  },
+  {
+    "revision": "ea6536b1b10c342afe60a615d29b650f",
+    "url": "/zomboji/static/media/zomboji-10.ea6536b1.png"
+  },
+  {
+    "revision": "9d9a1efa567aca3619cd9b2279f5f159",
+    "url": "/zomboji/static/media/zomboji-11.9d9a1efa.png"
+  },
+  {
+    "revision": "4686f04c5b0e747778a53592c98e0d67",
+    "url": "/zomboji/static/media/zomboji-12.4686f04c.png"
+  },
+  {
+    "revision": "94ef603c23494458cf3ac0daf250f426",
+    "url": "/zomboji/static/media/zomboji-13.94ef603c.png"
+  },
+  {
+    "revision": "4318651b63c14e636b362ff3053c8899",
+    "url": "/zomboji/static/media/zomboji-2.4318651b.png"
+  },
+  {
+    "revision": "8c83f03f7588f765971ecfa2248e0269",
+    "url": "/zomboji/static/media/zomboji-3.8c83f03f.png"
+  },
+  {
+    "revision": "486e760c8e2319e043d2dc1ea283a46b",
+    "url": "/zomboji/static/media/zomboji-4.486e760c.png"
+  },
+  {
+    "revision": "2e82e02b71426ebd445e585e23502439",
+    "url": "/zomboji/static/media/zomboji-5.2e82e02b.png"
+  },
+  {
+    "revision": "5ac3a2394cbe94898db4bcea8f8a3420",
+    "url": "/zomboji/static/media/zomboji-6.5ac3a239.png"
+  },
+  {
+    "revision": "79838a631e3663feed089030f9f5cd58",
+    "url": "/zomboji/static/media/zomboji-7.79838a63.png"
+  },
+  {
+    "revision": "f48ddce7732118c334521a904914bbce",
+    "url": "/zomboji/static/media/zomboji-8.f48ddce7.png"
+  },
+  {
+    "revision": "1d3c7f2426362285f611303e9392ee0d",
+    "url": "/zomboji/static/media/zomboji-9.1d3c7f24.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zomboji/precache-manifest.8bba2386b6e608aeb3ae22d0946c13e9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zomboji/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
